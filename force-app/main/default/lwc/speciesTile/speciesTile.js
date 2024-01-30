db55599562f503a1c1dc8a5a51b67900c7a81385
Jean Blanco <jeanc.blancor@unac.edu.co>
2024-01-30 18:29:49 -0500

    Lightning web compenent

---
+++ force-app/main/default/lwc/speciesTile/speciesTile.js
@@ -0,0 +1,11 @@
+import { LightningElement } from 'lwc';
+
+export default class SpeciesTile extends LightningElement {
+
+    specie = {
+        Name: "Jazmin",
+        Descripcion: "Olorosa y bonita planta trepadora",
+        imagen_URL__c: "https://www.hogarmania.com/archivos/202301/caracteristicas-cuidados-del-jazmin-848x477x80xX.jpg",
+        Localalizacion__c: "Indoors, Outdoors"
+    };
+}
