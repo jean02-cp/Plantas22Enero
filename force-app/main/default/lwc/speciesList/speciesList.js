6d2302aaebe601a4442c2e57da4a1c8158fb2296
Jean Blanco <jeanc.blancor@unac.edu.co>
2024-02-12 14:28:14 -0500

    ulima modificacion

--- force-app/main/default/lwc/speciesList/speciesList.js
+++ force-app/main/default/lwc/speciesList/speciesList.js
@@ -1,24 +1,17 @@
-import { LightningElement } from 'lwc';
-
+import { LightningElement, wire } from 'lwc';
+import getAllEspecies from "@salesforce/apex/SpeciesServis.getAllEspecies";
 export default class SpeciesList extends LightningElement {
-    species = [
-        {
-         Name: "Jazmin",
-         Descripcion_c: "Olorosa y bonita planta trepadora",
-         imagen_URL__c: "https://www.hogarmania.com/archivos/202301/caracteristicas-cuidados-del-jazmin-848x477x80xX.jpg",
-         Localalizacion__c: "Indoors, Outdoors"
-        },
-        {
-            Name: "Aloe vera",
-            Descripcion_c: "Conocida por sus propiedadees medicinales y aplicacion a la piel",
-            imagen_URL__c: "https://sicarfarms.com/wp-content/uploads/2021/01/Aloe-Vera.png",
-            Localalizacion__c: "Outdoors"
-        },
-        {
-            Name: "Hierbabuena",
-            Descripcion_c: "Aromatica  de huela de maravilla, muy usada enla gastronomia, en la medicina papular",
-            imagen_URL__c: "https://cdn0.ecologiaverde.com/es/posts/9/2/2/hierbabuena_propiedades_beneficios_y_como_tomar_4229_600.webp",
-            Localalizacion__c: "Outdoors"
-        }
-    ];
+
+    @wire(getAllEspecies)
+    species;
+    // specie.data --> datos devueltas por apex
+    // specie.error --> si error se almacena aqui
 }
+
+
+
+
+
+
+    
+   
--- force-app/main/default/lwc/speciesTile/speciesTile.js
+++ force-app/main/default/lwc/speciesTile/speciesTile.js
@@ -1,6 +1,8 @@
 import { LightningElement, api } from 'lwc';
 
 export default class SpeciesTile extends LightningElement {
+    @api specie;
 
-   @api specie;
+   
+
 }
